a72b9980ffdcd09a04242b1957f88dd22f4f709b
Patricia Rivas <pattycrivas96@gmail.com>
2023-12-06 00:17:59 -0300

    3era preentrega

--- JS/app.js
+++ JS/app.js
@@ -1,57 +1,141 @@
-const userName = prompt("Hola, para comenzar indicanos tu nombre :)");
-if (userName === "" || !isNaN(userName) || /\d/.test(userName)) {
-    alert("Favor ingresar un nombre valido")
-} else {
-    alert("Bienvenido/a a Pattyta Pizzeria " + userName + ", que ingredientes quieres elegir el dia de hoy?")
+const menuIngredientes = [
+    { nombre: "Queso", valor: 390, img: "../src/Queso.jpeg" },
+    { nombre: "Pepperoni", valor: 1290, img: "./src/Pepperoni.jpeg" },
+    { nombre: "Maiz", valor: 990, img: "./src/Maiz.jpeg" },
+    { nombre: "Jamón Serrano", valor: 1490, img: "./src/Jamon.jpeg" },
+    { nombre: "Aceitunas", valor: 790, img: "./src/Aceitunas.jpeg" },
+    { nombre: "Piña", valor: 490, img: "./src/Piña.jpeg" },
+    { nombre: "Tomate", valor: 390, img: "./src/Tomate.jpeg" },
+    { nombre: "Tocino", valor: 990, img: "./src/Tocino.jpeg" },
+];
 
-    const menuPizza = {
-        queso: 500,
-        pepperoni: 1000,
-        maiz: 250,
-        jamon: 300,
-    };
+const contenedor = document.getElementById("contenedorIngredientes");
+const carritoContenido = document.getElementById("carritoContenido");
+const carritoModal = new bootstrap.Modal(document.getElementById('carritoModal'));
+const repetirPedidoModal = new bootstrap.Modal(document.getElementById('repetirPedidoModal'));
 
-    let totalPedido = 0;
-    let pedido = [];
-    let propina = 0;
+function mostrarIngredientes() {
+    menuIngredientes.map(ingrediente => {
+        const divIngrediente = document.createElement("div");
+        divIngrediente.classList.add("col-md-3", "mb-4");
 
-    while (true) {
-        console.log("Menú Ingredientes:");
-        for (const pizza in menuPizza) {
-            console.log(`${pizza}: $${menuPizza[pizza]}`);
-        }
+        divIngrediente.innerHTML = `
+                <div class="card">
+                    <img src="${ingrediente.img}" class="card-img-top" alt="Imagen de ${ingrediente.nombre}">
+                    <div class="card-body">
+                        <h5 class="card-title">${ingrediente.nombre}</h5>
+                        <p class="card-text">Valor: ${ingrediente.valor}</p>
+                        <button class="btn btn-primary" onclick="agregarAlCarrito('${ingrediente.nombre}', ${ingrediente.valor})">Añadir a mi Pizza</button>
+                    </div>
+                </div>
+            `;
 
-        const eleccion = prompt("Elige tu ingrediente entre: queso, pepperoni, maiz, jamon o escribe 'fin' para terminar el pedido.");
+        contenedor.appendChild(divIngrediente);
+    });
+}
 
-        if (eleccion === 'fin') {
-            break;
-        }
+function agregarAlCarrito(nombre, valor) {
+    const nuevoElemento = document.createElement("div");
+    nuevoElemento.innerHTML = `
+          <p>${nombre} - Valor: ${valor} <button class="btn btn-danger btn-sm" onclick="eliminarIngrediente(this)">Eliminar</button></p>
+        `;
 
-        if (menuPizza[eleccion] !== undefined) {
-            pedido.push(eleccion);
-            totalPedido += menuPizza[eleccion];
-            alert(`Añadiste ${eleccion} a tu pedido.`);
-        } else {
-            alert("Esa ingrediente no está en el menú. Por favor, elige otro.");
-        }
-    }
+    carritoContenido.appendChild(nuevoElemento);
+    carritoModal.show();
+}
 
-    function calcularPropina(totalPedido) {
-        return totalPedido * 0.1;
-    }
+function eliminarIngrediente(elemento) {
+    elemento.parentNode.remove();
+}
 
-    propina = calcularPropina(totalPedido);
+function mostrarModalRepetirPedido() {
+    const ultimoPedidoJson = localStorage.getItem('ultimoPedido');
 
-    alert(`Tu pedido es: Pizza con ${pedido.join(", ")}`);
-    alert(`Total a pagar: $${totalPedido}`);
-    alert(`Propina (10%): $${propina}`);
-    alert(`Total a pagar con propina: $${totalPedido + propina}, gracias por tu compra :)`);
-
-    // Método filter y función para filtrar ingredientes caros
-    const ingredientesCaros = filtrarIngredientesCaros(pedido, menuPizza);
-    console.log('Ingredientes caros:', ingredientesCaros);
-
-    function filtrarIngredientesCaros(pedido, menuPizza) {
-        return pedido.filter(ingrediente => menuPizza[ingrediente] > 300);
+    if (ultimoPedidoJson) {
+        const ultimoPedido = JSON.parse(ultimoPedidoJson);
+        document.getElementById('repetirPedidoContenido').innerHTML = "";
+        ultimoPedido.map(ingrediente => {
+            const divIngrediente = document.createElement("div");
+            divIngrediente.innerHTML = `<p>${ingrediente.nombre} - Valor: ${ingrediente.valor}</p>`;
+            document.getElementById('repetirPedidoContenido').appendChild(divIngrediente);
+        });
+        repetirPedidoModal.show();
+    } else {
+        alert("No hay un pedido anterior almacenado.");
     }
 }
+
+function limpiarCarrito() {
+    carritoContenido.innerHTML = '';
+    repetirPedidoModal.hide();
+    localStorage.clear();
+}
+
+function finalizarPedido(idResultadoModal, deberCerrarModal = true) {
+    const elementosPedido = carritoContenido.getElementsByTagName("p");
+    const ingredientesPedido = [];
+
+    for (let i = 0; i < elementosPedido.length; i++) {
+        const nombreValorMatch = elementosPedido[i].innerText.match(/([^\d]+) - Valor: (\d+)/);
+        if (nombreValorMatch) {
+            const ingrediente = { nombre: nombreValorMatch[1], valor: parseInt(nombreValorMatch[2]) };
+            ingredientesPedido.push(ingrediente);
+        }
+    }
+
+    let totalPedido = 0;
+    ingredientesPedido.map(ingrediente => {
+        totalPedido += ingrediente.valor;
+    });
+
+    const propina = totalPedido * 0.1;
+    const totalPagar = totalPedido + propina;
+
+    // Guardar el nombre y valor del último pedido en el localStorage como JSON
+    localStorage.setItem('ultimoPedido', JSON.stringify(ingredientesPedido));
+
+    document.getElementById(idResultadoModal).innerHTML = `
+            <p>Total del Pedido: ${totalPedido}</p>
+            <p>Propina (10%): ${propina}</p>
+            <p>Total a Pagar: ${totalPagar}</p>
+        `;
+
+    if (deberCerrarModal) {
+        repetirPedidoModal.hide();
+    }
+}
+
+function filtrarPorPrecio() {
+    const filtroPrecio = document.getElementById("filtroPrecio").value;
+
+    if (filtroPrecio === "todos") {
+        contenedor.innerHTML = "";
+        mostrarIngredientes();
+    } else {
+        const ingredientesFiltrados = menuIngredientes.filter(ingrediente => ingrediente.valor < parseInt(filtroPrecio));
+        contenedor.innerHTML = "";
+        mostrarIngredientesFiltrados(ingredientesFiltrados);
+    }
+}
+
+function mostrarIngredientesFiltrados(ingredientes) {
+    ingredientes.map(ingrediente => {
+        const divIngrediente = document.createElement("div");
+        divIngrediente.classList.add("col-md-3", "mb-4");
+
+        divIngrediente.innerHTML = `
+                <div class="card">
+                    <img src="${ingrediente.img}" class="card-img-top" alt="Imagen de ${ingrediente.nombre}">
+                    <div class="card-body">
+                        <h5 class="card-title">${ingrediente.nombre}</h5>
+                        <p class="card-text">Valor: ${ingrediente.valor}</p>
+                        <button class="btn btn-primary" onclick="agregarAlCarrito('${ingrediente.nombre}', ${ingrediente.valor})">Añadir a mi Pizza</button>
+                    </div>
+                </div>
+            `;
+
+        contenedor.appendChild(divIngrediente);
+    });
+}
+
+window.onload = mostrarIngredientes;
